4ec02c9ed3934ed0756cc13f3815bc6e94913d5b
Grabner Mathias <grabne200054@sr.htlweiz.at>
2024-12-04 18:01:48 +0100

    changed structure

--- NTP.py
+++ NTP.py
@@ -1,4 +1,3 @@
-from time import localtime, strftime
 import struct
 
 class NTPPacket:
--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import socket
 import time
-from server import NTP
+import NTP
 
 
 
